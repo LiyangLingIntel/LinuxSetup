609fa9f14609b8c1d96a5ba54d36c809a84d7271
Ling, Liyang <liyang.ling@intel.com>
2022-06-09 10:00:37 +0800

    update treesitter supported langs

--- nvim/lua/configs/treesitter.lua
+++ nvim/lua/configs/treesitter.lua
@@ -3,7 +3,7 @@ function M.config()
     -- nvim-treesitter config
     require'nvim-treesitter.configs'.setup {
         -- ensure_installed = "maintained", -- for installing all maintained parsers
-        ensure_installed = { "c", "cpp", "cuda", "html", "json", "llvm", "lua", "python", "vim" }, -- for installing specific parsers
+        ensure_installed = { "bash", "c", "cpp", "cuda", "comment", "html", "json", "llvm", "lua", "python", "vim", "vue" }, -- for installing specific parsers
         sync_install = true, -- install synchronously
         ignore_install = { }, -- parsers to not install
         highlight = {
