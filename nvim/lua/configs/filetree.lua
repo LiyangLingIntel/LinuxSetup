c5260b8479e39f32ced3fe05986372f39820d02c
Ling, Liyang <liyang.ling@intel.com>
2023-06-14 00:30:10 -0700

    File tree update on focused file

--- nvim/lua/configs/filetree.lua
+++ nvim/lua/configs/filetree.lua
@@ -116,7 +116,7 @@ function M.config()
       auto_open = true,
     },
     update_focused_file = {
-      enable = false,
+      enable = true,
       update_root = false,
       ignore_list = {},
     },
